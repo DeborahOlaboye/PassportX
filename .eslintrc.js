46f7e68e0477993935a41a03d6ef66aafcbcb46a
sanmipaul <sanmipaul1@gmail.com>
2025-12-11 09:53:19 +0100

    feat: add code quality tools and linting configuration

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-console': 'warn',
+    'prefer-const': 'error',
+    'no-var': 'error'
+  },
+  env: {
+    node: true,
+    jest: true,
+    es6: true
+  }
+};
