0f81ad70892d8ef8581fb55db51f4a313a857494
DeborahOlaboye <deboraholaboye@gmail.com>
2025-12-23 03:48:15 +0100

    refactor: Update wallet components index with provider components

--- src/components/wallet/index.ts
+++ src/components/wallet/index.ts
@@ -5,3 +5,5 @@ export { default as AccountDisplay } from './AccountDisplay';
 export { default as ConnectionStatus } from './ConnectionStatus';
 export { default as WalletErrorBoundary } from './WalletErrorBoundary';
 export { default as WalletProvider } from './WalletProvider';
+export { default as ProviderInitializer } from './ProviderInitializer';
+export { default as EnhancedWalletProvider } from './EnhancedWalletProvider';
