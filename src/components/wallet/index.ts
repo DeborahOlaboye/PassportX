31b2a5dd7b7f7fb49ace7a8404a7a51055b5044b
DeborahOlaboye <deboraholaboye@gmail.com>
2025-12-23 03:40:04 +0100

    refactor: Add barrel export for wallet components

---
+++ src/components/wallet/index.ts
@@ -0,0 +1,5 @@
+export { default as ConnectButton } from './ConnectButton';
+export { default as WalletSelector } from './WalletSelector';
+export { default as QRCodeDisplay } from './QRCodeDisplay';
+export { default as AccountDisplay } from './AccountDisplay';
+export { default as ConnectionStatus } from './ConnectionStatus';
