fc320e9d58d1841da5e89db6db096f4c24834088
DeborahOlaboye <deboraholaboye@gmail.com>
2025-12-23 03:40:43 +0100

    refactor: Update wallet components index with error boundary export

--- src/components/wallet/index.ts
+++ src/components/wallet/index.ts
@@ -3,3 +3,4 @@ export { default as WalletSelector } from './WalletSelector';
 export { default as QRCodeDisplay } from './QRCodeDisplay';
 export { default as AccountDisplay } from './AccountDisplay';
 export { default as ConnectionStatus } from './ConnectionStatus';
+export { default as WalletErrorBoundary } from './WalletErrorBoundary';
