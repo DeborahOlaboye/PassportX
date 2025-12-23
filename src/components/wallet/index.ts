a4c1988e9e2338a0ae4bf4015666e3e8cd916cd9
DeborahOlaboye <deboraholaboye@gmail.com>
2025-12-23 03:41:06 +0100

    refactor: Update wallet components index with WalletProvider export

--- src/components/wallet/index.ts
+++ src/components/wallet/index.ts
@@ -4,3 +4,4 @@ export { default as QRCodeDisplay } from './QRCodeDisplay';
 export { default as AccountDisplay } from './AccountDisplay';
 export { default as ConnectionStatus } from './ConnectionStatus';
 export { default as WalletErrorBoundary } from './WalletErrorBoundary';
+export { default as WalletProvider } from './WalletProvider';
