b489c71ec670414c984122a2ddfd49639f24d0d8
sanmipaul <sanmipaul1@gmail.com>
2025-12-28 16:02:18 +0100

    issue-71: export error handling components from top-level index (11/15)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export { WalletSessionProvider } from './context/WalletSessionContext';
 export { useWalletSession } from './hooks/useWalletSession';
 export * from './utils/walletSession';
+export { ErrorBoundary } from './components/ErrorBoundary';
+export { default as ErrorToast } from './components/ErrorToast';
+export { default as FallbackUI } from './components/FallbackUI';
