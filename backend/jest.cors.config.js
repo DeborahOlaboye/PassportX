5a3d9d5211160322f31352fa96d107db769849fc
0xMosas <tosinolosasa@gmail.com>
2026-01-27 07:55:53 +0100

    test(cors): add isolated jest config for CORS tests
    - Configure standalone jest config to avoid test setup conflicts
    - Enable running CORS tests independently
    
    Related to #143

---
+++ backend/jest.cors.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/cors.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+  ],
+  setupFilesAfterEnv: [],
+  globalSetup: undefined,
+  globalTeardown: undefined,
+};
