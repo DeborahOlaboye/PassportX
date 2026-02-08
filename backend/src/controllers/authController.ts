94ec7f2f78eb18cacc3b5ea33875c2a3e7993cf0
DeborahOlaboye <deboraholaboye@gmail.com>
2026-02-08 06:36:34 +0100

    fix(auth): remove unsafe type casting in authController
    - Replace (user as any).passportId with user.passportId
    - Use proper type-safe property access
    - Maintain same functionality without type bypass
    
    Related to #151

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -75,7 +75,7 @@ export const authenticateWithWallet = async (req: Request, res: Response) => {
           email: user.email,
           isPublic: user.isPublic,
           joinDate: user.joinDate,
-          hasPassport: !!(user as any).passportId,
+          hasPassport: !!user.passportId,
           communities: user.communities,
           adminCommunities: user.adminCommunities
         }
@@ -129,7 +129,7 @@ export const verifySession = async (req: Request, res: Response) => {
           email: user.email,
           isPublic: user.isPublic,
           joinDate: user.joinDate,
-          hasPassport: !!(user as any).passportId,
+          hasPassport: !!user.passportId,
           communities: user.communities,
           adminCommunities: user.adminCommunities
         }
