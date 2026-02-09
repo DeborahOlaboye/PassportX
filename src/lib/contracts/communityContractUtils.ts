d6791a73fb6869b14573b52387d4a177282a95d7
DeborahOlaboye <deboraholaboye@gmail.com>
2026-02-09 05:50:53 +0100

    refactor(contracts): import TransactionStatusResponse in communityContractUtils

--- src/lib/contracts/communityContractUtils.ts
+++ src/lib/contracts/communityContractUtils.ts
@@ -12,7 +12,7 @@ import {
   contractPrincipalCV
 } from '@stacks/transactions';
 import { StacksTestnet, StacksMainnet } from '@stacks/network';
-import { BaseContractResponse } from '@/types/contract';
+import { BaseContractResponse, TransactionStatusResponse } from '@/types/contract';
 
 export interface CommunityMetadata {
   name: string;
