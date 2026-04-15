216c034d7d8ab2c46dedf568f6dc40ed74814983
DeborahOlaboye <deboraholaboye@gmail.com>
2026-04-15 06:46:56 +0100

    Remove existing coverageThreshold to update configuration

--- jest.config.js
+++ jest.config.js
@@ -6,13 +6,5 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.{js,ts}', '!src/**/*.d.ts', '!src/index.ts'],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'html'],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
   setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
 };
